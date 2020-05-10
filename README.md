9517c6f74e7bb5d1b0a7d65aa7691c9bfb07b5e7
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2020-05-10 22:30:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Process Monitor
+
+## Requirements:
+
+`pip install psutils`
+
+## Help:
+
+```
+python process_monitor.py --help
+```
+
+## Examples:
+
+```
+python process_monitor.py 1254 -s
+```
---
+++ process_monitor.py
@@ -0,0 +1,75 @@
+import csv
+
+
+def get_size(bytes: float) -> str:
+    '''
+    Returns size of bytes in a nice format
+    '''
+    for unit in ['', 'K', 'M', 'G', 'T', 'P']:
+        if bytes < 1024:
+            return f'{bytes:.2f} {unit}B'
+        bytes /= 1024
+
+
+def table_row(data: list) -> str:
+    row_format = '{:>15}' * len(data)
+    return row_format.format(*data)
+
+
+def append_row(file: str, data: list):
+    with open(file, 'a', newline='') as fd:
+        writer = csv.writer(fd, delimiter=';')
+        writer.writerow(data)
+
+
+if __name__ == '__main__':
+    import argparse
+    import datetime
+    import psutil
+    import time
+
+    parser = argparse.ArgumentParser(description='Process utilization monitor')
+    parser.add_argument(dest='pid', type=int, help='PID of monitored process.')
+    parser.add_argument('-s', '--stdout', dest='std_output', action='store_true', help='Print results to screen.')
+    parser.add_argument('-o', '--output', help='File to write the results (in CSV format).',
+                        default='{dt}-PID{pid}.csv')
+
+    # parse arguments
+    args = parser.parse_args()
+    output_file = args.output.format(dt=datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S'), pid=args.pid)
+
+    total_mhz = psutil.cpu_freq().max
+    if_counters_prev = psutil.net_io_counters()
+    try:
+        p = psutil.Process(args.pid)
+
+        if args.std_output:
+            print('Process name:', p.name())
+            print(table_row(['CPU', 'Virtual Mem', 'Used Mem', 'Network Sent', 'Network Recv']))
+        else:
+            append_row(output_file, ['CPU, MHz', 'Virtual Mem, bytes', 'Used Mem, bytes',
+                                     'Network Sent, bytes', 'Network Recv, bytes'])
+
+        while True:
+            with p.oneshot():
+                mem = p.memory_full_info()
+                mhz = p.cpu_percent() * total_mhz / 100
+                if_counters = psutil.net_io_counters()
+                if_sent = if_counters.bytes_sent - if_counters_prev.bytes_sent
+                if_recv = if_counters.bytes_recv - if_counters_prev.bytes_recv
+
+            if_counters_prev = if_counters
+
+            if args.std_output:
+                print(table_row([
+                    f'{mhz} MHz', get_size(mem.vms), get_size(mem.uss),
+                    get_size(if_sent),
+                    get_size(if_recv)
+                ]))
+            else:
+                append_row(output_file, [mhz, mem.vms, mem.uss, if_sent, if_recv])
+
+            time.sleep(1)
+
+    except psutil.NoSuchProcess as ex:
+        print(ex)
