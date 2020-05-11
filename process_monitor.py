eb48b58d56bff43ba7690ea348da98a7c01e6bcf
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2020-05-11 20:35:15 +0300

    Docstring style

--- process_monitor.py
+++ process_monitor.py
@@ -2,9 +2,9 @@ import csv
 
 
 def get_size(bytes: float) -> str:
-    '''
+    """
     Returns size of bytes in a nice format
-    '''
+    """
     for unit in ['', 'K', 'M', 'G', 'T', 'P']:
         if bytes < 1024:
             return f'{bytes:.2f} {unit}B'
